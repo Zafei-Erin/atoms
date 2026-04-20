6b3f580f4536c0543c7c95da9adcc575217cd40b
Zafei-Erin <897659752@qq.com>
2026-04-20 17:56:09 +0800

    Update wps.ts

--- backend/src/routes/wps.ts
+++ backend/src/routes/wps.ts
@@ -25,8 +25,7 @@ wpsRouter.get("/files/:fileId/download", async (c) => {
   return c.json({
     code: 0,
     data: {
-      download_url:
-        "https://doc-review.tos-cn-beijing.volces.com/%E9%99%84%E4%BB%B63-%E4%B8%AD%E5%9B%BD%E5%B9%B3%E5%AE%89%E9%87%91%E8%A3%95%E4%BA%BA%E7%94%9F%E7%90%86%E8%B4%A2%E4%BA%A7%E5%93%81-f1689605-cfd0-4246-bd86-375a38526fce-1776655921492.docx?X-Tos-Algorithm=TOS4-HMAC-SHA256&X-Tos-Content-Sha256=UNSIGNED-PAYLOAD&X-Tos-Credential=AKTP23R3oKR4PnicVxMBiuxex21w36zc7zYLVc93N9b6zHk%2F20260420%2Fcn-beijing%2Ftos%2Frequest&X-Tos-Date=20260420T094805Z&X-Tos-Expires=3600&X-Tos-SignedHeaders=host&X-Tos-Security-Token=nChBvMlNIdFphUGtMcUtHWld2.CiQKEHVNU1JGYmN4M1BjUm9iVlMSEMsgl20pb0eOqCJCSwl9zIAQo9-XzwYYs_uXzwYgqvvC6wcoATCq-8LrBzoEcm9vdEIDdG9zUhcwNDg45omL5py655So5oi3I01xVEhwTVgBYAE.JTJEa9MlxQqMHwhUxBWaBt7w1YQH4EWSo8IeZ8utNRDn8tQ9_EykNfED3U6oqi8qnG_KqJ36jMGXH3tmp0fuXg&X-Tos-Signature=ddcf9d942439a635f1e425299b23558d908c3c157b990285fd6cbd8237635bfc",
+      url: "https://doc-review.tos-cn-beijing.volces.com/%E9%99%84%E4%BB%B63-%E4%B8%AD%E5%9B%BD%E5%B9%B3%E5%AE%89%E9%87%91%E8%A3%95%E4%BA%BA%E7%94%9F%E7%90%86%E8%B4%A2%E4%BA%A7%E5%93%81-f1689605-cfd0-4246-bd86-375a38526fce-1776655921492.docx?X-Tos-Algorithm=TOS4-HMAC-SHA256&X-Tos-Content-Sha256=UNSIGNED-PAYLOAD&X-Tos-Credential=AKTP23R3oKR4PnicVxMBiuxex21w36zc7zYLVc93N9b6zHk%2F20260420%2Fcn-beijing%2Ftos%2Frequest&X-Tos-Date=20260420T094805Z&X-Tos-Expires=3600&X-Tos-SignedHeaders=host&X-Tos-Security-Token=nChBvMlNIdFphUGtMcUtHWld2.CiQKEHVNU1JGYmN4M1BjUm9iVlMSEMsgl20pb0eOqCJCSwl9zIAQo9-XzwYYs_uXzwYgqvvC6wcoATCq-8LrBzoEcm9vdEIDdG9zUhcwNDg45omL5py655So5oi3I01xVEhwTVgBYAE.JTJEa9MlxQqMHwhUxBWaBt7w1YQH4EWSo8IeZ8utNRDn8tQ9_EykNfED3U6oqi8qnG_KqJ36jMGXH3tmp0fuXg&X-Tos-Signature=ddcf9d942439a635f1e425299b23558d908c3c157b990285fd6cbd8237635bfc",
     },
   });
 });
